3587582948431a017255f5e8335fa38e5aecf2db
庄天翼 <zty0826@gmail.com>
2020-05-26 19:08:10 +0800

    dashboard,config: support dashboard dynamic public path prefix (#2454)

---
+++ pkg/dashboard/uiserver/embedded_assets_rewriter.go
@@ -0,0 +1,39 @@
+package uiserver
+
+import (
+	"html"
+	"os"
+	"strings"
+)
+
+type modifiedFileInfo struct {
+	os.FileInfo
+	size int64
+}
+
+func (f modifiedFileInfo) Size() int64 {
+	return f.size
+}
+
+func (f modifiedFileInfo) Sys() interface{} {
+	return nil
+}
+
+// InitAssetFS init the static resources with given public path prefix.
+func InitAssetFS(prefix string) {
+	rewrite := func(assetPath string) {
+		a, err := _bindata[assetPath]()
+		if err != nil {
+			panic("Asset " + assetPath + " not found.")
+		}
+		tmplText := string(a.bytes)
+		updated := strings.ReplaceAll(tmplText, "__PUBLIC_PATH_PREFIX__", html.EscapeString(prefix))
+		a.bytes = []byte(updated)
+		a.info = modifiedFileInfo{a.info, int64(len(a.bytes))}
+		_bindata[assetPath] = func() (*asset, error) {
+			return a, nil
+		}
+	}
+	rewrite("build/index.html")
+	rewrite("build/diagnoseReport.html")
+}
