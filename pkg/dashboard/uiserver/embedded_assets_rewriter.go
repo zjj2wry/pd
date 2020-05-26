91f1688c971f88b41effac6e67773eb602d4adf6
Wenxuan <hi@breeswish.org>
2020-05-27 00:09:42 +0800

    Add TiDB Dashboard configurations (#2458)
    Signed-off-by: Breezewish <me@breeswish.org>

--- pkg/dashboard/uiserver/embedded_assets_rewriter.go
+++ pkg/dashboard/uiserver/embedded_assets_rewriter.go
@@ -1,10 +1,26 @@
+// Copyright 2020 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package uiserver
 
 import (
 	"html"
 	"os"
 	"strings"
+	"sync"
 )
+
+var once sync.Once
 
 type modifiedFileInfo struct {
 	os.FileInfo
@@ -21,19 +37,21 @@ func (f modifiedFileInfo) Sys() interface{} {
 
 // InitAssetFS init the static resources with given public path prefix.
 func InitAssetFS(prefix string) {
-	rewrite := func(assetPath string) {
-		a, err := _bindata[assetPath]()
-		if err != nil {
-			panic("Asset " + assetPath + " not found.")
+	once.Do(func() {
+		rewrite := func(assetPath string) {
+			a, err := _bindata[assetPath]()
+			if err != nil {
+				panic("Asset " + assetPath + " not found.")
+			}
+			tmplText := string(a.bytes)
+			updated := strings.ReplaceAll(tmplText, "__PUBLIC_PATH_PREFIX__", html.EscapeString(prefix))
+			a.bytes = []byte(updated)
+			a.info = modifiedFileInfo{a.info, int64(len(a.bytes))}
+			_bindata[assetPath] = func() (*asset, error) {
+				return a, nil
+			}
 		}
-		tmplText := string(a.bytes)
-		updated := strings.ReplaceAll(tmplText, "__PUBLIC_PATH_PREFIX__", html.EscapeString(prefix))
-		a.bytes = []byte(updated)
-		a.info = modifiedFileInfo{a.info, int64(len(a.bytes))}
-		_bindata[assetPath] = func() (*asset, error) {
-			return a, nil
-		}
-	}
-	rewrite("build/index.html")
-	rewrite("build/diagnoseReport.html")
+		rewrite("build/index.html")
+		rewrite("build/diagnoseReport.html")
+	})
 }
