85c8a6d9eea1d4eb1b75dbe942416c01ee3b909f
Song Gao <disxiaofei@163.com>
2020-06-30 19:56:11 +0800

    test: improve coverage for rule.go  (#2589)
    * add set unit test
    
    * add test cases
    
    * address the comment
    
    Co-authored-by: ti-srebot <66930949+ti-srebot@users.noreply.github.com>

--- server/api/rule_test.go
+++ server/api/rule_test.go
@@ -17,6 +17,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	. "github.com/pingcap/check"
 	"github.com/pingcap/pd/v4/server"
@@ -39,80 +40,313 @@ func (s *testRuleSuite) SetUpSuite(c *C) {
 	s.urlPrefix = fmt.Sprintf("%s%s/api/v1/config", addr, apiPrefix)
 
 	mustBootstrapCluster(c, s.svr)
+	PDServerCfg := s.svr.GetConfig().PDServerCfg
+	PDServerCfg.KeyType = "raw"
+	err := s.svr.SetPDServerConfig(PDServerCfg)
+	c.Assert(err, IsNil)
+	c.Assert(postJSON(testDialClient, s.urlPrefix, []byte(`{"enable-placement-rules":"true"}`)), IsNil)
 }
 
 func (s *testRuleSuite) TearDownSuite(c *C) {
 	s.cleanup()
 }
 
-func (s *testRuleSuite) Testrule(c *C) {
-	PDServerCfg := s.svr.GetConfig().PDServerCfg
-	PDServerCfg.KeyType = "raw"
-	err := s.svr.SetPDServerConfig(PDServerCfg)
+func (s *testRuleSuite) TestSet(c *C) {
+	rule := placement.Rule{GroupID: "a", ID: "10", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1}
+	successData, err := json.Marshal(rule)
 	c.Assert(err, IsNil)
-	c.Assert(postJSON(testDialClient, s.urlPrefix, []byte(`{"enable-placement-rules":"true"}`)), IsNil)
-	rule1 := placement.Rule{GroupID: "a", ID: "10", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1}
-	rule2 := placement.Rule{GroupID: "a", ID: "20", StartKeyHex: "3333", EndKeyHex: "5555", Role: "voter", Count: 2}
-	rule3 := placement.Rule{GroupID: "b", ID: "20", StartKeyHex: "5555", EndKeyHex: "7777", Role: "voter", Count: 3}
-
-	//Set
-	postData, err := json.Marshal(rule1)
+	parseErrData := []byte("foo")
+	rule1 := placement.Rule{GroupID: "a", ID: "10", StartKeyHex: "XXXX", EndKeyHex: "3333", Role: "voter", Count: 1}
+	checkErrData, err := json.Marshal(rule1)
 	c.Assert(err, IsNil)
-	err = postJSON(testDialClient, s.urlPrefix+"/rule", postData)
-	c.Assert(err, IsNil)
-	postData, err = json.Marshal(rule2)
-	c.Assert(err, IsNil)
-	err = postJSON(testDialClient, s.urlPrefix+"/rule", postData)
-	c.Assert(err, IsNil)
-	postData, err = json.Marshal(rule3)
-	c.Assert(err, IsNil)
-	err = postJSON(testDialClient, s.urlPrefix+"/rule", postData)
+	rule2 := placement.Rule{GroupID: "a", ID: "10", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: -1}
+	setErrData, err := json.Marshal(rule2)
 	c.Assert(err, IsNil)
 
-	//Get
-	var resp placement.Rule
-	err = readJSON(testDialClient, s.urlPrefix+"/rule/a/10", &resp)
-	c.Assert(err, IsNil)
-	compareRule(c, &resp, &rule1)
+	testcases := []struct {
+		name     string
+		rawData  []byte
+		success  bool
+		response string
+	}{
+		{
+			name:     "Set rule success",
+			rawData:  successData,
+			success:  true,
+			response: "",
+		},
+		{
+			name:    "Parse Json failed",
+			rawData: parseErrData,
+			success: false,
+			response: `{
+  "code": "input",
+  "msg": "invalid character 'o' in literal false (expecting 'a')",
+  "data": {
+    "Offset": 2
+  }
+}
+`,
+		},
+		{
+			name:    "Check rule failed",
+			rawData: checkErrData,
+			success: false,
+			response: `"start key is not in hex format: encoding/hex: invalid byte: U+0058 'X'"
+`,
+		},
+		{
+			name:    "Set Rule Failed",
+			rawData: setErrData,
+			success: false,
+			response: `"invalid count -1"
+`,
+		},
+	}
 
-	//GetAll
+	for _, testcase := range testcases {
+		c.Log(testcase.name)
+		err = postJSON(testDialClient, s.urlPrefix+"/rule", testcase.rawData)
+		if testcase.success {
+			c.Assert(err, IsNil)
+		} else {
+			c.Assert(err, NotNil)
+			c.Assert(err.Error(), Equals, testcase.response)
+		}
+	}
+}
+
+func (s *testRuleSuite) TestGet(c *C) {
+	rule := placement.Rule{GroupID: "a", ID: "20", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1}
+	data, err := json.Marshal(rule)
+	c.Assert(err, IsNil)
+	err = postJSON(testDialClient, s.urlPrefix+"/rule", data)
+	c.Assert(err, IsNil)
+
+	testcases := []struct {
+		name  string
+		rule  placement.Rule
+		found bool
+		code  string
+	}{
+		{
+			name:  "found",
+			rule:  rule,
+			found: true,
+			code:  "",
+		},
+		{
+			name:  "not found",
+			rule:  placement.Rule{GroupID: "a", ID: "30", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1},
+			found: false,
+			code:  "404",
+		},
+	}
+	for _, testcase := range testcases {
+		c.Log(testcase.name)
+		var resp placement.Rule
+		url := fmt.Sprintf("%s/rule/%s/%s", s.urlPrefix, testcase.rule.GroupID, testcase.rule.ID)
+		err = readJSON(testDialClient, url, &resp)
+		if testcase.found {
+			c.Assert(err, IsNil)
+			compareRule(c, &resp, &testcase.rule)
+		} else {
+			c.Assert(err, NotNil)
+			c.Assert(strings.HasSuffix(err.Error(), testcase.code), Equals, true)
+		}
+	}
+}
+
+func (s *testRuleSuite) TestGetAll(c *C) {
+	rule := placement.Rule{GroupID: "b", ID: "20", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1}
+	data, err := json.Marshal(rule)
+	c.Assert(err, IsNil)
+	err = postJSON(testDialClient, s.urlPrefix+"/rule", data)
+	c.Assert(err, IsNil)
+
 	var resp2 []*placement.Rule
 	err = readJSON(testDialClient, s.urlPrefix+"/rules", &resp2)
 	c.Assert(err, IsNil)
-	c.Assert(len(resp2), Equals, 4)
+	c.Assert(len(resp2), GreaterEqual, 1)
+}
 
-	//GetAllByGroup
-	err = readJSON(testDialClient, s.urlPrefix+"/rules/group/b", &resp2)
+func (s *testRuleSuite) TestGetAllByGroup(c *C) {
+	rule := placement.Rule{GroupID: "c", ID: "20", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1}
+	data, err := json.Marshal(rule)
 	c.Assert(err, IsNil)
-	c.Assert(len(resp2), Equals, 1)
-	compareRule(c, resp2[0], &rule3)
+	err = postJSON(testDialClient, s.urlPrefix+"/rule", data)
+	c.Assert(err, IsNil)
 
-	//GetAllByRegion
+	rule1 := placement.Rule{GroupID: "c", ID: "30", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1}
+	data, err = json.Marshal(rule1)
+	c.Assert(err, IsNil)
+	err = postJSON(testDialClient, s.urlPrefix+"/rule", data)
+	c.Assert(err, IsNil)
+
+	testcases := []struct {
+		name    string
+		groupID string
+		count   int
+	}{
+		{
+			name:    "found group c",
+			groupID: "c",
+			count:   2,
+		},
+		{
+			name:    "not found d",
+			groupID: "d",
+			count:   0,
+		},
+	}
+
+	for _, testcase := range testcases {
+		c.Log(testcase.name)
+		var resp []*placement.Rule
+		url := fmt.Sprintf("%s/rules/group/%s", s.urlPrefix, testcase.groupID)
+		err = readJSON(testDialClient, url, &resp)
+		c.Assert(err, IsNil)
+		c.Assert(len(resp), Equals, testcase.count)
+		if testcase.count == 2 {
+			compareRule(c, resp[0], &rule)
+			compareRule(c, resp[1], &rule1)
+		}
+	}
+}
+
+func (s *testRuleSuite) TestGetAllByRegion(c *C) {
+	rule := placement.Rule{GroupID: "e", ID: "20", StartKeyHex: "1111", EndKeyHex: "3333", Role: "voter", Count: 1}
+	data, err := json.Marshal(rule)
+	c.Assert(err, IsNil)
+	err = postJSON(testDialClient, s.urlPrefix+"/rule", data)
+	c.Assert(err, IsNil)
+
 	r := newTestRegionInfo(4, 1, []byte([]byte{0x22, 0x22}), []byte{0x33, 0x33})
 	mustRegionHeartbeat(c, s.svr, r)
-	err = readJSON(testDialClient, s.urlPrefix+"/rules/region/4", &resp2)
+
+	testcases := []struct {
+		name     string
+		regionID string
+		success  bool
+		code     string
+	}{
+		{
+			name:     "found region",
+			regionID: "4",
+			success:  true,
+		},
+		{
+			name:     "parse regionId failed",
+			regionID: "abc",
+			success:  false,
+			code:     "400",
+		},
+		{
+			name:     "region not found",
+			regionID: "5",
+			success:  false,
+			code:     "404",
+		},
+	}
+	for _, testcase := range testcases {
+		c.Log(testcase.name)
+		var resp []*placement.Rule
+		url := fmt.Sprintf("%s/rules/region/%s", s.urlPrefix, testcase.regionID)
+		err = readJSON(testDialClient, url, &resp)
+		if testcase.success {
+			c.Assert(err, IsNil)
+			for _, r := range resp {
+				if r.GroupID == "e" {
+					compareRule(c, r, &rule)
+				}
+			}
+		} else {
+			c.Assert(err, NotNil)
+			c.Assert(strings.HasSuffix(err.Error(), testcase.code), Equals, true)
+		}
+	}
+}
+
+func (s *testRuleSuite) TestGetAllByKey(c *C) {
+	rule := placement.Rule{GroupID: "f", ID: "40", StartKeyHex: "8888", EndKeyHex: "9111", Role: "voter", Count: 1}
+	data, err := json.Marshal(rule)
 	c.Assert(err, IsNil)
-	c.Assert(len(resp2), Equals, 2)
-	if resp2[0].GroupID == "pd" {
-		compareRule(c, resp2[1], &rule1)
-	} else {
-		compareRule(c, resp2[0], &rule1)
+	err = postJSON(testDialClient, s.urlPrefix+"/rule", data)
+	c.Assert(err, IsNil)
+
+	testcases := []struct {
+		name     string
+		key      string
+		success  bool
+		respSize int
+		code     string
+	}{
+		{
+			name:     "key in range",
+			key:      "8899",
+			success:  true,
+			respSize: 2,
+		},
+		{
+			name:     "parse key failed",
+			key:      "abc",
+			success:  false,
+			code:     "400",
+			respSize: 0,
+		},
+		{
+			name:     "key out of range",
+			key:      "9999",
+			success:  true,
+			respSize: 1,
+		},
 	}
 
-	//GetAllByKey
-	err = readJSON(testDialClient, s.urlPrefix+"/rules/key/4444", &resp2)
-	c.Assert(err, IsNil)
-	c.Assert(len(resp2), Equals, 2)
-	if resp2[0].GroupID == "pd" {
-		compareRule(c, resp2[1], &rule2)
-	} else {
-		compareRule(c, resp2[0], &rule2)
+	for _, testcase := range testcases {
+		c.Log(testcase.name)
+		var resp []*placement.Rule
+		url := fmt.Sprintf("%s/rules/key/%s", s.urlPrefix, testcase.key)
+		err = readJSON(testDialClient, url, &resp)
+		if testcase.success {
+			c.Assert(err, IsNil)
+			c.Assert(len(resp), Equals, testcase.respSize)
+		} else {
+			c.Assert(err, NotNil)
+			c.Assert(strings.HasSuffix(err.Error(), testcase.code), Equals, true)
+		}
 	}
+}
 
-	//Delete
-	resp3, err := doDelete(testDialClient, s.urlPrefix+"/rule/a/10")
+func (s *testRuleSuite) TestDelete(c *C) {
+	rule := placement.Rule{GroupID: "g", ID: "10", StartKeyHex: "8888", EndKeyHex: "9111", Role: "voter", Count: 1}
+	data, err := json.Marshal(rule)
 	c.Assert(err, IsNil)
-	c.Assert(resp3.StatusCode, Equals, http.StatusOK)
+	err = postJSON(testDialClient, s.urlPrefix+"/rule", data)
+	c.Assert(err, IsNil)
+
+	testcases := []struct {
+		name    string
+		groupID string
+		id      string
+	}{
+		{
+			name:    "delete existed rule",
+			groupID: "g",
+			id:      "10",
+		},
+		{
+			name:    "delete non-existed rule",
+			groupID: "g",
+			id:      "15",
+		},
+	}
+	for _, testcase := range testcases {
+		c.Log(testcase.name)
+		url := fmt.Sprintf("%s/rule/%s/%s", s.urlPrefix, testcase.groupID, testcase.id)
+		resp, err := doDelete(testDialClient, url)
+		c.Assert(err, IsNil)
+		c.Assert(resp.StatusCode, Equals, http.StatusOK)
+	}
 }
 
 func compareRule(c *C, r1 *placement.Rule, r2 *placement.Rule) {
